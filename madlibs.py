358112ffb49514895c0236962cc36e2ca3d3a7d7
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-07-20 05:23:47 -0400

    comment madlibs.py, fix issue #1
    unfortunate solution, I'll have to figure out a better one later

--- madlibs.py
+++ madlibs.py
@@ -10,16 +10,14 @@ types = {
 }    
 
 def getRandom(typ):
-    typ = types[typ]
     return str(words[typ][rand.randint(0,len(words[typ]))])
 
 def madlibs(phrase):
     for t in types:
-        for i in range(phrase.count(t)):
-            phrase = phrase.replace(t, ' ' + getRandom(t) + ' ',1)
-    return phrase.strip().replace('  ',' ')
+        for i in range(phrase.count(t)): ## yes this has to be done, otherwise it just replaces all occurrences with the same word
+            phrase = phrase.replace(t, ' ' + getRandom(types[t]) + ' ',1)
+    return phrase.strip().replace('  ',' ') ## just doing some cleanup just in case :shrug:
 
 
 
-while True:
-    print(madlibs(input()))
+print(madlibs(input()))
