07c751c7ec584ddcf5985e1f8f8ab032a9696d5e
raimaaan03 <43918793+raimaaan03@users.noreply.github.com>
2020-07-21 03:55:53 -0400

    Update site and make functional
    god this is some ugly shit, but at least I'm getting it to work now, after a good 7 hours of hitting my head against async fetch() until I learned I can import frameworks using <script> tags :facepalm:

---
+++ web/main.js
@@ -0,0 +1,41 @@
+const types = {
+    '[j]':'adjective',
+    '[n]':'noun',
+    '[v]':'verb',
+    '[a]':'adverb'
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function getRandom(typ){
+//	console.log(words['noun'][123]);
+	let parts = words[typ];
+//	console.log(parts);
+	word = parts[getRndInteger(0, parts.length-1)];
+//	console.log(word);
+	return word;
+}
+
+function processMadlibs(A){
+//	console.log(words['noun'][123]);
+	output = A;
+	for(type in types){
+//		console.log(types[type]);
+//		console.log(getRandom(type));
+		while(output.includes(type)){
+//			console.log(type)
+			output = output.replace(type, getRandom(types[type]));
+		}
+	}
+	return String(output);
+}
+
+function madlibs(A){
+//	console.log(words)
+//	console.log('madlibs('+ A +')');
+	var result = processMadlibs(A);
+	console.log(result);
+	return result;
+}
